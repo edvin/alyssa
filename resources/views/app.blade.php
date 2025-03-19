3559ef2b0d3abe4234d6120b312c7ba62a2def4b
Edvin Syse <edvin.syse@group.one>
2025-03-19 15:37:45 +0100

    Wrap logo in link to /

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
     @vite('resources/js/app.js')
     @vite('resources/css/app.css')
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -11,11 +11,13 @@
     @inertiaHead
 </head>
 <body>
-<header class="pt-2 bg-gray-100 shadow-md border-b-gray-400 flex items-center gap-8 pl-10">
-    <img src="/images/logo.png" class="shadow-gray-700 shadow-2xl rounded-full h-14 w-14 mb-2"/>
-    <span class="font-[Stalemate] text-5xl font-bold text-red-700">
+<header class="pt-2 bg-gray-100 shadow-md border-b-gray-400">
+    <a href="/" class="flex items-center gap-8 pl-10">
+        <img src="/images/logo.png" class="shadow-gray-700 shadow-2xl rounded-full h-14 w-14 mb-2"/>
+        <span class="font-[Stalemate] text-5xl font-bold text-red-700">
         Dagens Alyssa
     </span>
+    </a>
 </header>
 @inertia
 </body>
