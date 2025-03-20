9676e4d5ec9c2329273f7190b24cecb169fdf6d5
Edvin Syse <edvin.syse@group.one>
2025-03-20 09:32:35 +0100

    Extract QuoteSymbol to it's own component, because Turge complained

--- resources/js/Components/Quote.vue
+++ resources/js/Components/Quote.vue
@@ -1,6 +1,8 @@
 <script setup>
-import {computed, onMounted} from "vue";
+import {computed} from "vue";
 import {Link} from "@inertiajs/vue3";
+import QuoteSymbol from "./QuoteSymbol.vue";
+
 const props = defineProps({
     quote: {
         type: Object
@@ -28,15 +30,12 @@ const age = computed(() => {
 
 <template>
     <div class="flex place-content-center pt-16 w-full">
-        <section class="bg-slate-50 w-11/12 md:w-1/2 border-l-gray-300 border-l-10 pl-5 py-16 flex place-content-center text-lg">
+        <section
+            class="bg-slate-50 w-11/12 md:w-1/2 border-l-gray-300 border-l-10 pl-5 py-16 flex place-content-center text-lg">
 
             <blockquote>
                 <div>
-                    <svg class="w-8 h-8 text-gray-400 dark:text-gray-600 mb-4" aria-hidden="true"
-                         xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 14">
-                        <path
-                            d="M6 0H2a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3H2a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Zm10 0h-4a2 2 0 0 0-2 2v4a2 2 0 0 0 2 2h4v1a3 3 0 0 1-3 3h-1a1 1 0 0 0 0 2h1a5.006 5.006 0 0 0 5-5V2a2 2 0 0 0-2-2Z"/>
-                    </svg>
+                    <QuoteSymbol/>
                     <span v-html="quote.text"/>
                 </div>
                 <small>- Alyssa, {{ age }} år</small>
